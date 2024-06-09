5721453a5abd625fb2dc765ddafba4c7a8591992
Sergi Casquero <111904117+Sergi-CM@users.noreply.github.com>
2024-06-09 20:04:15 +0200

    Add Footer component

---
+++ src/components/Footer/socialMediaLinks.ts
@@ -0,0 +1,22 @@
+const socialMediaLinks = [
+  {
+    name: "facebook",
+    url: "https://www.facebook.com/jardineriacanadell",
+    icon: "icons/facebook.svg",
+    alt: "Facebook",
+  },
+  {
+    name: "instagram",
+    url: "https://www.instagram.com/jardineriacanadell",
+    icon: "icons/instagram.svg",
+    alt: "Instagram",
+  },
+  {
+    name: "whatsapp",
+    url: "https://wa.me/34621254391",
+    icon: "icons/whatsapp.svg",
+    alt: "Whatsapp",
+  },
+];
+
+export default socialMediaLinks
