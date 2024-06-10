80361f47777e1ab36f6bcabeed7215d596969350
Sergi Casquero <111904117+Sergi-CM@users.noreply.github.com>
2024-06-10 22:41:42 +0200

    Update main page

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as Footer } from "./Footer/Footer.astro";
+export { default as SocialMediaLink } from "./SocialMediaLink/SocialMediaLink.astro";
+export { default as Header } from "./Header/Header.astro";
+export { default as Hero } from "./Hero/Hero.astro";
+export { default as SectionTitle } from "./SectionTitle/SectionTitle.astro";
+export { default as Welcome } from "./Welcome/Welcome.astro";
+export { default as ServicesSection } from "./ServicesSection/ServicesSection.astro";
+export { default as ServiceCard } from "./ServiceCard/ServiceCard.astro";
